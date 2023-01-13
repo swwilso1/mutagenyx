09eccdaf3b25581c42c313ce2f9d418443e493a9
Steve Wilson <steve@certora.com>
2023-01-13 06:38:08 -0700

    Add initial comments to gambit_lib::ast.rs.

--- gambit_lib/src/ast.rs
+++ gambit_lib/src/ast.rs
@@ -1,6 +1,13 @@
 use crate::visitor::*;
 
+/// The trait that describes the functionality that an object must possess in order to
+/// traverse an abstract syntax tree.
 pub trait AST<N> {
+    /// Traverse a language abstract syntax tree allowing the visitor object access to each node
+    /// during the traversal.  The traversal algorithm does not mutate the AST.
     fn traverse<V: Visitor<N> + ?Sized>(&self, visitor: &mut V) -> bool;
+
+    /// Traverse a language abstract syntax tree allowing the visitor object access to each node
+    /// during the traversal.  The visitor object may mutate each node.
     fn traverse_mut<V: VisitorMut<N> + ?Sized>(&mut self, visitor: &mut V) -> bool;
 }
