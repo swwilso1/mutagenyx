c4238a9bdc7981f7633914cc7158d0fd1d949621
Steve Wilson <steve@certora.com>
2023-01-18 15:37:20 -0700

    Document gambit_lib/src/solidity/visitor.rs.

--- gambit_lib/src/solidity/visitor.rs
+++ gambit_lib/src/solidity/visitor.rs
@@ -1,3 +1,6 @@
+//! The `solidity::visitor` module contains visitors for counting mutable nodes in a Solidity
+//! AST and visitors for mutating nodes in the AST.
+
 use crate::mutation::MutationType;
 use crate::mutator::*;
 use crate::solidity::ast::SolidityAST;
@@ -5,12 +8,22 @@ use crate::visitor::*;
 use rand_pcg::*;
 use std::collections::HashMap;
 
+/// [`Visitor`] conforming object that exists to count the number of nodes in the AST that match
+/// various mutation algorithms.
 pub struct SolidityMutationNodeCounter<'a> {
+    /// The table that holds the number of mutable nodes for each mutation type.
     pub counter_table: HashMap<MutationType, u64>,
+
+    /// A reference to the map that maps mutation algorithm types to the mutators that implement
+    /// the algorithms.
     mutator_map: &'a HashMap<MutationType, Box<dyn Mutator<SolidityAST>>>,
 }
 
 impl<'a> SolidityMutationNodeCounter<'a> {
+    /// Create a new visitor.
+    ///
+    /// # Arguments
+    /// * `mutator_map` - A reference to the map of mutators.
     pub fn new(
         mutator_map: &'a HashMap<MutationType, Box<dyn Mutator<SolidityAST>>>,
     ) -> SolidityMutationNodeCounter<'a> {
@@ -49,14 +62,29 @@ impl<'a> Visitor<SolidityAST> for SolidityMutationNodeCounter<'a> {
     }
 }
 
+/// [`VisitorMut`] conforming object that exists to mutate a node of the Solidity AST.
 pub struct SolidityMutationMaker<'a> {
+    /// A reference to the mutator that will mutate the node.
     mutator: &'a Box<dyn Mutator<SolidityAST>>,
+
+    /// A reference to a random number generator that the mutator can use to perform the mutation.
     rng: &'a mut Pcg64,
+
+    /// The index of the node in the AST to mutate.
     index: usize,
+
+    /// The number of mutable nodes (mutable by the mutator) already found in the AST.
     current_index: usize,
 }
 
 impl<'a> SolidityMutationMaker<'a> {
+    /// Create the new visitor.
+    ///
+    /// # Arguments
+    ///
+    /// * `mutator` - The object that will mutate the node.
+    /// * `rng` - The random number generator object for use by the `mutator`.
+    /// * `index` - The index of the node to mutate.
     pub fn new(
         mutator: &'a Box<dyn Mutator<SolidityAST>>,
         rng: &'a mut Pcg64,
