967ad31ea061003486f3f04d87e99b7d24b270f2
Steve Wilson <steve@certora.com>
2023-01-30 12:19:37 -0700

    Add documentation for Preferences code.

--- metamorph_lib/src/preferences.rs
+++ metamorph_lib/src/preferences.rs
@@ -2,6 +2,7 @@ use crate::json::*;
 use serde_json::{from_str, json, Value};
 use std::collections::HashMap;
 
+/// Value type stored in the preferences object.
 #[derive(Debug)]
 pub enum PreferenceValue<'a> {
     Int(i64),
@@ -13,23 +14,37 @@ pub enum PreferenceValue<'a> {
     Null,
 }
 
+/// Preferences object that provides a key/value store where the value can have multiple sub-types.
 #[derive(Debug)]
 pub struct Preferences {
+    /// A JSON object.
     data: Value,
 }
 
 impl Preferences {
+    /// Create a new [`Preferences`] object.
     pub fn new() -> Preferences {
         Preferences {
             data: from_str("{}").unwrap(),
         }
     }
 
+    /// Insert a new key/value pair, or update the value for a preexisting key/value pair.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - The string value containing the key.
+    /// * `value` - The stored value.
     pub fn set_value_for_key(&mut self, key: &str, value: PreferenceValue) {
         let json_value = Preferences::convert_preference_value_to_json(&value);
         self.data.set_node_for_key(key, json_value);
     }
 
+    /// Get the value for a key.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - The string value containing the key.
     pub fn get_value_for_key(&self, key: &str) -> Option<PreferenceValue> {
         if let Some(node) = self.data.borrow_value_for_key(key) {
             return Some(Preferences::convert_json_to_preference_value(node));
@@ -37,6 +52,11 @@ impl Preferences {
         None
     }
 
+    /// Return true if the preferences contains a value for `key`.
+    ///
+    /// # Arguments
+    ///
+    /// * `key` - The string value containing the key.
     pub fn contains_key(&self, key: &str) -> bool {
         if let Some(_node) = self.data.borrow_value_for_key(key) {
             return true;
@@ -44,6 +64,8 @@ impl Preferences {
         false
     }
 
+    /// Helper function to convert a JSON [`Value`] object to a [`PreferenceValue`]
+    /// analogue.
     fn convert_json_to_preference_value(jvalue: &Value) -> PreferenceValue {
         if jvalue.is_i64() {
             PreferenceValue::Int(jvalue.as_i64().unwrap())
@@ -79,13 +101,14 @@ impl Preferences {
         }
     }
 
+    /// Helper function to convert a [`PreferenceValue`] to a JSON [`Value`] object.
     fn convert_preference_value_to_json(value: &PreferenceValue) -> Value {
         match value {
             PreferenceValue::Int(i) => json![i],
             PreferenceValue::Float(f) => json![f],
             PreferenceValue::String(s) => json![s],
             PreferenceValue::Boolean(b) => json![b],
-            PreferenceValue::Null => from_str("").unwrap(),
+            PreferenceValue::Null => Value::Null,
             PreferenceValue::Object(map) => {
                 let mut json_object: Value = from_str("{}").unwrap();
                 for (key, value) in map {
